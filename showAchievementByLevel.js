384d50c52af72b426c73233fea2070bcca60ad7b
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-01-08 02:34:05 +0900

    Update showAchievementByLevel.js

--- showAchievementByLevel.js
+++ showAchievementByLevel.js
@@ -26,75 +26,6 @@ export function main(){
         'EXPERT 9'
     ];
 
-    // 挿入するHTMLの雛形
-    const template = `
-    <div class="playdata__playerdata">
-        <div class="playdata__score-point">  
-            <ul class="playdata__score-point__wrap">
-                <li>
-                <div class="diff_icon___difficulty__">__stageLevel__</div>
-                
-                </li><li>
-                <div>
-                    <img src="/img/web/play_data/icon/c_txt_clear.png">
-                </div>
-                <p class="score-point__difficulty difficulty__all is-show">__clear__/
-                    <span class="score-point__difficulty difficulty__all is-show">__stageCount__</span>
-                </p>
-                </li>
-                <li>
-                <div>
-                    <img src="/img/web/play_data/icon/c_txt_s.png">
-                </div>
-                <p class="score-point__difficulty difficulty__all is-show">__s__/
-                    <span class="score-point__difficulty difficulty__all is-show">__stageCount__</span>
-                </p>
-                </li>
-                <li>
-                <div>
-                    <img src="/img/web/play_data/icon/c_txt_missless.png">
-                </div>
-                <p class="score-point__difficulty difficulty__all is-show">__missless__/
-                    <span class="score-point__difficulty difficulty__all is-show">__stageCount__</span>
-                </p>
-                </li>
-                <li>
-                <div>
-                    <img src="/img/web/play_data/icon/c_txt_ss.png">
-                </div>
-                <p class="score-point__difficulty difficulty__all is-show">__ss__/
-                    <span class="score-point__difficulty difficulty__all is-show">__stageCount__</span>
-                </p>
-                </li>
-                <li>
-                <div>
-                    <img src="/img/web/play_data/icon/c_txt_full.png">
-                </div>
-                <p class="score-point__difficulty difficulty__all is-show">__fullcombo__/
-                    <span class="score-point__difficulty difficulty__all is-show">__stageCount__</span>
-                </p>
-                </li>
-                <li>
-                <div>
-                    <img src="/img/web/play_data/icon/c_txt_sss.png">
-                </div>
-                <p class="score-point__difficulty difficulty__all is-show">__sss__/
-                    <span class="score-point__difficulty difficulty__all is-show">__stageCount__</span>
-                </p>
-                </li>
-                <li>
-                <div>
-                    <img src="/img/web/play_data/icon/c_txt_all_m.png">
-                </div>
-                <p class="score-point__difficulty difficulty__all is-show">__allmarvelous__/
-                    <span class="score-point__difficulty difficulty__all is-show">__stageCount__</span>
-                </p>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <hr>`;
-
     // 集計結果を入れるための配列
     let result = {};
 
@@ -105,6 +36,8 @@ export function main(){
             's' : 0,
             'ss' : 0,
             'sss' : 0,
+            '990k' : 0,
+            '995k' : 0,
             'clear' : 0,
             'missless' : 0,
             'fullcombo' : 0,
@@ -120,6 +53,7 @@ export function main(){
         difficulty.forEach(function(d){
             // '!d!'は難易度。難易度ごとに別のクラス名となっているため、パラメーター式にノードを選択する。
             let stageLevel = String(s.querySelector(String('div.difficulty__!d! div.playdata__score-list__song-info__lv').replace('!d!', d)).innerText).trim();
+            let playScore = s.querySelector(String('div.difficulty__!d! div.playdata__score-list__song-info__score').replace('!d!', d)).innerText.match(/[0-9]+/g);
             let rateIcon = s.querySelectorAll(String('div.score__icon__!d! img').replace('!d!', d))[0].src.match(/[^/]+?$/g);
             let achieveIcon = s.querySelectorAll(String('div.score__icon__!d! img').replace('!d!', d))[1].src.match(/[^/]+?$/g);
             // 譜面レベルに記載のあるもののみ処理
@@ -131,7 +65,9 @@ export function main(){
                 if (rateIcon != 'no_rate.svg'){
                     if (Number(String(rateIcon).match(/[0-9]+/g)) >= 7) result[stageLevel]['s'] += 1;
                     if (Number(String(rateIcon).match(/[0-9]+/g)) >= 8) result[stageLevel]['ss'] += 1;
-                    if (Number(String(rateIcon).match(/[0-9]+/g)) >= 9) result[stageLevel]['sss'] += 1
+                    if (Number(String(rateIcon).match(/[0-9]+/g)) >= 9) result[stageLevel]['sss'] += 1;
+                    if (Number(playScore) >= 990000) result[stageLevel]['990k'] += 1;
+                    if (Number(playScore) >= 995000) result[stageLevel]['995k'] += 1
                 };
 
                 // コンボ評価に応じて譜面数を1加算
@@ -150,23 +86,123 @@ export function main(){
     // 集計結果をもとにHTMLのコードを生成
     let insertCode = '';
     level.forEach(function(l){
-        let tmp = template;
+        const success = ` style="color: deeppink; font-weight: bold"`
+        let
+            difficulty = '',
+            stageLevel = l,
+            stageCount = result[l]['stageCount'],
+            rateS = result[l]['s'],
+            styleS = '',
+            rateSS = result[l]['ss'],
+            styleSS = '',
+            rateSSS = result[l]['sss'],
+            styleSSS = '',
+            rate990k = result[l]['990k'],
+            style990k = '',
+            rate995k = result[l]['995k'],
+            style995k = '',
+            clear = result[l]['clear'],
+            styleClear = '',
+            missless = result[l]['missless'],
+            styleMissless = '',
+            fullcombo = result[l]['fullcombo'],
+            styleFullcombo = '',
+            allmarvelous = result[l]['allmarvelous'],
+            styleAllmarvelous = '';
+
         if (l.indexOf('INFERNO') != -1){
-            tmp = tmp.replaceAll('__difficulty__', 'inferno')
+            difficulty = 'inferno'
         } else {
-            tmp = tmp.replaceAll('__difficulty__', 'expert')
+            difficulty = 'expert'
         };
-        tmp = tmp.replaceAll('__stageLevel__', l);
-        tmp = tmp.replaceAll('__stageCount__', result[l]['stageCount']);
-        tmp = tmp.replaceAll('__s__', result[l]['s']);
-        tmp = tmp.replaceAll('__ss__', result[l]['ss']);
-        tmp = tmp.replaceAll('__sss__', result[l]['sss']);
-        tmp = tmp.replaceAll('__clear__', result[l]['clear']);
-        tmp = tmp.replaceAll('__missless__', result[l]['missless']);
-        tmp = tmp.replaceAll('__fullcombo__', result[l]['fullcombo']);
-        tmp = tmp.replaceAll('__allmarvelous__', result[l]['allmarvelous']);
+
+        if (stageCount == rateS) styleS = success;
+
+        if (stageCount == rateSS) styleSS = success;
+
+        if (stageCount == rateSSS) styleSSS = success;
+
+        if (stageCount == rate990k) style990k = success;
+
+        if (stageCount == rate995k) style995k = success;
+
+        if (stageCount == clear) styleClear = success;
+
+        if (stageCount == missless) styleMissless = success;
+
+        if (stageCount == fullcombo) styleFullcombo = success;
+
+        if (stageCount == allmarvelous) styleAllmarvelous = success;
+
         if (insertCode != '') insertCode += '\n';
-        insertCode += tmp
+
+        insertCode +=  `
+            <div class="playdata__playerdata">
+                <div class="playdata__score-point">  
+                    <ul class="playdata__score-point__wrap">
+                        <li>
+                            <div class="diff_icon_${difficulty}">${stageLevel}</div>
+                        </li>
+                        <li>
+                        <div>
+                            <img src="/img/web/play_data/icon/c_txt_clear.png">
+                        </div>
+                        <p class="score-point__difficulty difficulty__all is-show"${styleClear}>${clear}/
+                            <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
+                        </p>
+                        </li>
+                        <li>
+                        <div>
+                            <img src="/img/web/play_data/icon/c_txt_s.png">
+                        </div>
+                        <p class="score-point__difficulty difficulty__all is-show"${styleS}>${rateS}/
+                            <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
+                        </p>
+                        </li>
+                        <li>
+                        <div>
+                            <img src="/img/web/play_data/icon/c_txt_missless.png">
+                        </div>
+                        <p class="score-point__difficulty difficulty__all is-show"${styleMissless}>${missless}/
+                            <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
+                        </p>
+                        </li>
+                        <li>
+                        <div>
+                            <img src="/img/web/play_data/icon/c_txt_ss.png">
+                        </div>
+                        <p class="score-point__difficulty difficulty__all is-show"${styleSS}>${rateSS}/
+                            <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
+                        </p>
+                        </li>
+                        <li>
+                        <div>
+                            <img src="/img/web/play_data/icon/c_txt_full.png">
+                        </div>
+                        <p class="score-point__difficulty difficulty__all is-show"${styleFullcombo}>${fullcombo}/
+                            <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
+                        </p>
+                        </li>
+                        <li>
+                        <div>
+                            <img src="/img/web/play_data/icon/c_txt_sss.png">
+                        </div>
+                        <p class="score-point__difficulty difficulty__all is-show"${styleSSS}>${rateSSS}/
+                            <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
+                        </p>
+                        </li>
+                        <li>
+                        <div>
+                            <img src="/img/web/play_data/icon/c_txt_all_m.png">
+                        </div>
+                        <p class="score-point__difficulty difficulty__all is-show"${styleAllmarvelous}>${allmarvelous}/
+                            <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
+                        </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <hr>`;
     });
 
     // 集計結果を挿入
