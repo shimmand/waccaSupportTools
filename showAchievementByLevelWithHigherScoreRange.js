26cfaba86d4cf7012af9e7116f4332b6a3ab6fc2
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-08-15 04:49:26 +0900

    Update showAchievementByLevelWithHigherScoreRange.js

--- showAchievementByLevelWithHigherScoreRange.js
+++ showAchievementByLevelWithHigherScoreRange.js
@@ -71,49 +71,49 @@ export function main(){
                     // 900k以上？
                     if (Number(playScore) >= 900000){
                         result[stageLevel]['s'] += 1;
-                        s.setAttribute(`${stageLevelClass}_rate_s`, '1');
+                        s.setAttribute(`${stageLevelClass}_rate_s`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_rate_s`, '2');
+                        s.setAttribute(`${stageLevelClass}_rate_s`, 0);
                     };
 
                     // 930k以上？
                     if (Number(playScore) >= 930000){
                         result[stageLevel]['s_plus'] += 1;
-                        s.setAttribute(`${stageLevelClass}_rate_s_plus`, '1');
+                        s.setAttribute(`${stageLevelClass}_rate_s_plus`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_rate_s_plus`, '2');
+                        s.setAttribute(`${stageLevelClass}_rate_s_plus`, 0);
                     };
 
                     // 950k以上？
                     if (Number(playScore) >= 950000){
                         result[stageLevel]['ss'] += 1;
-                        s.setAttribute(`${stageLevelClass}_rate_ss`, '1');
+                        s.setAttribute(`${stageLevelClass}_rate_ss`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_rate_ss`, '2');
+                        s.setAttribute(`${stageLevelClass}_rate_ss`, 0);
                     };
 
                     // 970k以上？
                     if (Number(playScore) >= 970000){
                         result[stageLevel]['ss_plus'] += 1;
-                        s.setAttribute(`${stageLevelClass}_rate_ss_plus`, '1');
+                        s.setAttribute(`${stageLevelClass}_rate_ss_plus`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_rate_ss_plus`, '2');
+                        s.setAttribute(`${stageLevelClass}_rate_ss_plus`, 0);
                     };
 
                     // 980k以上？
                     if (Number(playScore) >= 980000){
                         result[stageLevel]['sss'] += 1;
-                        s.setAttribute(`${stageLevelClass}_rate_sss`, '1');
+                        s.setAttribute(`${stageLevelClass}_rate_sss`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_rate_sss`, '2');
+                        s.setAttribute(`${stageLevelClass}_rate_sss`, 0);
                     };
 
                     // 990k以上？
                     if (Number(playScore) >= 990000){
                         result[stageLevel]['sss_plus'] += 1;
-                        s.setAttribute(`${stageLevelClass}_rate_sss_plus`, '1');
+                        s.setAttribute(`${stageLevelClass}_rate_sss_plus`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_rate_sss_plus`, '2');
+                        s.setAttribute(`${stageLevelClass}_rate_sss_plus`, 0);
                     };
                 };
 
@@ -121,30 +121,30 @@ export function main(){
                 if (achieveIcon != 'no_achieve.svg'){
                     if (Number(String(achieveIcon).match(/[0-9]+/g)) >= 1){
                         result[stageLevel]['clear'] += 1;
-                        s.setAttribute(`${stageLevelClass}_clear`, '1');
+                        s.setAttribute(`${stageLevelClass}_clear`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_clear`, '2');
+                        s.setAttribute(`${stageLevelClass}_clear`, 0);
                     };
 
                     if (Number(String(achieveIcon).match(/[0-9]+/g)) >= 2){
                         result[stageLevel]['missless'] += 1;
-                        s.setAttribute(`${stageLevelClass}_missless`, '1');
+                        s.setAttribute(`${stageLevelClass}_missless`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_missless`, '2');
+                        s.setAttribute(`${stageLevelClass}_missless`, 0);
                     };
 
                     if (Number(String(achieveIcon).match(/[0-9]+/g)) >= 3){
                         result[stageLevel]['fullcombo'] += 1;
-                        s.setAttribute(`${stageLevelClass}_fullcombo`, '1');
+                        s.setAttribute(`${stageLevelClass}_fullcombo`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_fullcombo`, '2');
+                        s.setAttribute(`${stageLevelClass}_fullcombo`, 0);
                     };
 
                     if (Number(String(achieveIcon).match(/[0-9]+/g)) == 4){
                         result[stageLevel]['allmarvelous'] += 1;
-                        s.setAttribute(`${stageLevelClass}_allmarvelous`, '1');
+                        s.setAttribute(`${stageLevelClass}_allmarvelous`, 1);
                     } else {
-                        s.setAttribute(`${stageLevelClass}_allmarvelous`, '2');
+                        s.setAttribute(`${stageLevelClass}_allmarvelous`, 0);
                     };
                 };
             };
@@ -224,7 +224,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_clear.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_clear') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_clear') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleClear}>${clear}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -235,7 +235,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_s.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_s') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_s') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleS}>${rateS}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -246,7 +246,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_s_plus.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_s_plus') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_s_plus') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleSP}>${rateSP}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -257,7 +257,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_missless.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_missless') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_missless') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleMissless}>${missless}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -268,7 +268,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_ss.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_ss') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_ss') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleSS}>${rateSS}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -279,7 +279,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_ss_plus.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_ss_plus') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_ss_plus') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleSSP}>${rateSSP}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -290,7 +290,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_full.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_fullcombo') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_fullcombo') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleFullcombo}>${fullcombo}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -301,7 +301,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_sss.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_sss') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_sss') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleSSS}>${rateSSS}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -312,7 +312,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_sss_plus.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_sss_plus') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_rate_sss_plus') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleSSSP}>${rateSSSP}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -323,7 +323,7 @@ export function main(){
                             <div>
                                 <img src="/img/web/play_data/icon/c_txt_all_m.png">
                             </div>
-                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_allmarvelous') == 2}); location.href = '#isNormal'">
+                            <a onclick="javascript:${click} grid.filter(function (item) { var element = item.getElement(); return element.getAttribute('${stageLevelClass}_allmarvelous') == document.querySelectorAll('input.filter-option')[0].checked}); location.href = '#isNormal'">
                                 <p class="score-point__difficulty difficulty__all is-show"${styleAllmarvelous}>${allmarvelous}/
                                     <span class="score-point__difficulty difficulty__all is-show">${stageCount}</span>
                                 </p>
@@ -338,12 +338,21 @@ export function main(){
     // 集計結果を挿入
     document.querySelector('div.playdata__filter-btn').insertAdjacentHTML('beforebegin', insertCode);
 
-    const info = `<div style="text-align: center; font-size: 0.8em;">
-                    <p style="font-weight: bold;">達成状況ビューアー v1.02</p>
+    const info = `<div style="text-align: left; font-size: 0.8em;">
+                    <p style="font-weight: bold; padding: 10px 0 10px;">達成状況ビューアー v1.02</p>
+                    <p>- 各項目の譜面数をタップすると、譜面の一覧でフィルターが適用され、該当の譜面のみが表示されます。</p>
+                    <p>- フィルターの条件は、以下の「フィルター設定」で変更することができます。</p>
+                    <p>- 難易度タブを切り替えたり、デフォルトの「フィルタ」ボタンでフィルターを適用したりすると、前述のフィルターは解除されます。</p>
+                    <p>- 元の画面に戻すには、このページを再読み込みするか、他のページへ移動します。</p>
                     <hr>
-                    <p>元の画面に戻すには、ページを再読み込みしてください。</p>
-                    <p>譜面数をタップすると、その条件を未達成の譜面がリスト表示されます。</p>
-                    <p>（未達成の譜面がない場合は表示されません）</p>
+                    <p style="font-weight: bold; padding: 10px 0 10px;">フィルター設定</p>
+                    <div style="padding: 0 0 10px;">
+                        <input type="radio" id="achieved" name="filter-option" class="filter-option" checked>
+                        <label for="achieved">達成済みの譜面を表示</label>
+                        <input type="radio" id="unachieved" name="filter-option" class="filter-option">
+                        <label for="unachieved">未達成の譜面を表示</label>
+                    </div>
+                    <p>- 上記のラジオボタンを変更すると、この後に譜面数をタップした時から反映されます。</p>
                 </div>
                 <hr>`;
     document.querySelector('div.contents-wrap').insertAdjacentHTML('afterbegin', info);
