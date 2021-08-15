21c2d20262b9e2fffffdf97628ec5190f592d24b
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-08-16 04:21:30 +0900

    Create light.js

---
+++ toolsCollectionForPlayHistory/light.js
@@ -0,0 +1,15 @@
+export function main(){
+    function hideAllTimes(){
+        // #pushobj > section > div.contents-wrap > div > ul > li:nth-child(any) > div > div.playdata__history-list__song-info > div.playdata__history-list__song-info__top
+        const selectors = 'div.playdata__history-list__song-info__top';
+        const datesPlayed = document.querySelectorAll(selectors);
+        datesPlayed.forEach(function(e){
+            const searchValue = /[0-9]{2}:[0-9]{2}:[0-9]{2}$/;
+            const replaceValue = '##:##:##';
+            const modifiedCode = e.innerHTML.replace(searchValue, replaceValue);
+            e.innerHTML = modifiedCode;
+        });
+    };
+    
+    hideAllTimes();
+};
