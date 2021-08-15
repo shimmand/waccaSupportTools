f9caf76e2d6dc8ba87c1a350f34f084789405c5f
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-08-16 03:57:12 +0900

    Create toolsCollectionForPlayHistory.js

---
+++ toolsCollectionForPlayHistory.js
@@ -0,0 +1,25 @@
+export function main(){
+    function hideAllTimes(){
+        // #pushobj > section > div.contents-wrap > div > ul > li:nth-child(any) > div > div.playdata__history-list__song-info > div.playdata__history-list__song-info__top
+        const selectors = 'div.playdata__history-list__song-info__top';
+        const datesPlayed = document.querySelectorAll(selectors);
+        datesPlayed.forEach(function(e){
+            const searchValue = /[0-9]{2}:[0-9]{2}:[0-9]{2}$/;
+            const replaceValue = '##:##:##';
+            const modifiedCode = e.innerHTML.replace(searchValue, replaceValue);
+            e.innerHTML = modifiedCode;
+        });
+    };
+
+    function showAllTables(){
+        // #pushobj > section > div.contents-wrap > div > ul > li:nth-child(any) > div > a
+        const selectors = 'a.playdata__history-list__detail-btn';
+        const datesPlayed = document.querySelectorAll(selectors);
+        datesPlayed.forEach(function(e){
+            e.click();
+        });
+    };
+
+    hideAllTimes();
+    showAllTables();
+};
