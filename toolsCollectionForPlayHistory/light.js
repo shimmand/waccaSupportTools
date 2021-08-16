3f93375daa1664aedbe89c058ce0ad408728d834
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-08-16 23:33:47 +0900

    Update light.js

--- toolsCollectionForPlayHistory/light.js
+++ toolsCollectionForPlayHistory/light.js
@@ -2,8 +2,8 @@ export function main(){
     function hideAllTimes(){
         // #pushobj > section > div.contents-wrap > div > ul > li:nth-child(any) > div > div.playdata__history-list__song-info > div.playdata__history-list__song-info__top
         const selectors = 'div.playdata__history-list__song-info__top';
-        const datesPlayed = document.querySelectorAll(selectors);
-        datesPlayed.forEach(function(e){
+        const datailedTables = document.querySelectorAll(selectors);
+        datailedTables.forEach(function(e){
             const searchValue = /[0-9]{2}:[0-9]{2}:[0-9]{2}$/;
             const replaceValue = '##:##:##';
             const modifiedCode = e.innerHTML.replace(searchValue, replaceValue);
