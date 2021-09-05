f3d6fa582f9c9726883a538443b650fa9f101941
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-09-06 00:21:19 +0900

    Update main.js

--- showRatingInfo/main.js
+++ showRatingInfo/main.js
@@ -1,1 +1,24 @@
+function getCSV(dataPath) {
+    const request = new XMLHttpRequest();
 
+    request.addEventListener('load', (event) => {
+        const response = event.target.responseText;
+        toArray(response);
+    });
+
+    request.open('GET', dataPath, true);
+    request.send();
+};
+
+function toArray(data) {
+    const dataArray = [];
+    const dataString = data.split('\n');
+
+    for (let i = 0; i < dataString.length; i++) {
+        dataArray[i] = dataString[i].split(',');
+    };
+
+    console.log(dataArray);
+};
+
+getCSV('https://shimmand.github.io/wacca_support_tools/chartsData/latest.csv');
