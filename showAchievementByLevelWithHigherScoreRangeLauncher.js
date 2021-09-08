e43dc7a97c0d6ac9d7ae0189ba198b8b5edefcb5
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-09-09 02:16:55 +0900

    Update showAchievementByLevelWithHigherScoreRangeLauncher.js

--- showAchievementByLevelWithHigherScoreRangeLauncher.js
+++ showAchievementByLevelWithHigherScoreRangeLauncher.js
@@ -1,1 +1,1 @@
-javascript:(function(){ let path = 'https://shimmand.github.io/wacca_support_tools/showAchievementByLevelWithHigherScoreRange.js'; import(path).then((obj) => obj.main())})();
+javascript:(function(){ let path = 'https://shimmand.github.io/waccaSupportTools/showAchievementByLevelWithHigherScoreRange.js'; import(path).then((obj) => obj.main())})();
