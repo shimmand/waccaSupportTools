07669917cb570975f650cfe0b39040f3d619d73d
SHIMMAND <58391224+shimmand@users.noreply.github.com>
2021-10-29 00:18:57 +0900

    Update main.js

--- analyzePlayData/dev/main.js
+++ analyzePlayData/dev/main.js
@@ -56,7 +56,7 @@ export function main(){
     const openMainPage = `javascript:setData=document.querySelector('#scoresList').value;if(navigator.clipboard==undefined){window.clipboardData.setData('Text',setData);}else{navigator.clipboard.writeText(setData);};window.open('https://shimmand.github.io/waccaSupportTools/analyzePlayData/rating.html');`;
     const insertCode = 
     `<div style="text-align: left; font-size: 0.8em; padding: 20px">
-        <p style="font-weight: bold; padding: 10px 0 10px;">WACCA RATING ANALYZER v1.00</p>
+        <p style="font-weight: bold; padding: 10px 0 10px;">WACCA RATING ANALYZER v1.01</p>
         <p>スコアの取得が完了しました。</p>
         <p>計算を開始するには、以下のボタンを押してください。</p>
         <p>The web program is now ready to run.</p>
